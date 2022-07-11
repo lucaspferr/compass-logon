2a5f70d565f222a5d8cee63da8bc4c76d346e0c0
Lucas Pimentel <lucas.pferr194@gmail.com>
2022-07-11 12:38:05 -0300

    Added Weather component

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Clock from './components/Clock/Clock'
 import LogonCompass from './components/containers/main_content/LogonCompass'
+import Weather from './components/Weather/Weather'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <LogonCompass/> */}
     <Clock/>
+    <Weather/>
   </React.StrictMode>
 )
