1f3dc523106d77e890cb8f9997b4a5c9af317c45
Lucas Pimentel <lucas.pferr194@gmail.com>
2022-07-14 17:37:55 -0300

    Storybook created

---
+++ src/components/Countdown/Counterdown.stories.jsx
@@ -0,0 +1,31 @@
+import { CounterContainer, CounterText, CounterTimer, Seconds } from "./styles";
+
+function Countdown() {
+  return (
+    <CounterContainer
+      style={{
+        background: "linear-gradient(90.16deg,#33383D 0%,#1C1D20 100%)",
+      }}
+    >
+      <CounterText>
+        Application
+        <br />
+        refresh in
+      </CounterText>
+      <CounterTimer style={{ marginTop: "10px" }}>600</CounterTimer>
+      <Seconds>seconds</Seconds>
+    </CounterContainer>
+  );
+}
+
+export default {
+  title: "Components/Countdown",
+  component: Countdown,
+};
+
+const Template = (args) => <Countdown {...args} />;
+
+export const CountdownWithContext = () => {
+  title: "Countdown 600 seconds";
+  return <Countdown />;
+};
